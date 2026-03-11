e2a407d45b92216838bc50c42c7a5dd476d4d91e
KOBAYASHI Atsushi <kobaatsu@gmail.com>
2026-03-11 12:00:55 +0900

    build: replace @vitejs/plugin-basic-ssl with vite-plugin-mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react(), mkcert()],
   test: {
     globals: true,
     environment: 'jsdom',
